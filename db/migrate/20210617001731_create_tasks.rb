dbe1705bf42b6da2b4002c69ba9890f536abeff4
Shujaat Azim <shujaat.azim@gmail.com>
2021-07-07 19:36:12 -0400

    Found grammatical error in default task despcription, fixed it.

--- db/migrate/20210617001731_create_tasks.rb
+++ db/migrate/20210617001731_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.text :description, default: "Add a description of this task!"
+      t.text :description, default: "Add a description for this task!"
       t.string :status
       t.references :project, null: false, foreign_key: true
 
